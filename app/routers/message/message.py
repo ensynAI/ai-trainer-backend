23dff991b7f3a4e0fa154525bf984d872c0c207b
JakubTomaszewski <jakub.k.tomaszewski@gmail.com>
2023-12-10 17:07:55 +0100

    CSAT-36 Feat: Add `/message/initialize` endpoint responsible for initializing a new conversation with a bot.
    The endpoint still has some TODO's, like getting the input messages from the database.

--- app/routers/message/message.py
+++ app/routers/message/message.py
@@ -10,27 +10,29 @@ router = APIRouter(
 
 @router.post("/generate")
 def generate_message(messages: list[Message]) -> dict[str, Message]:
-    response = chatbot_client.send_request(messages)
-    new_message = chatbot_client.process_response(response)
-
+    new_message = chatbot_client.generate_response_message(messages)
     return {"message": new_message}
 
 
-# @app.post("/initial")
-# def initialize_conversation(user_login: str):
+@router.post("/initialize")
+def initialize_conversation() -> dict[str, list[Message]]:
     # Makes a call to the database
-    # Based on the user_login retrieves the company details
-    # i.e. the input phrase to the SYSTEM model
+    # Based on the user_login retrieves the company details and hence the initial prompt message
+    # Returns the initial conversation message
 
-    # Calls the prompt builder
+    # TODO:
+    # system_message_content = db.get_system_prompt(user_login)
+    # assistant_message_content = db.get_assistant_prompt(user_login)
 
-    # Makes a call to the Model API initializing the conversation
-    # returns the initial message to the client
-    # ...
-    # This functionality can be also implemented in the /generate_message endpoint
+    system_message = "You are a fake customer for the purposes of training customer service agents by practising with you. Act like a customer having a problem with one of the products you have bought. Only generate responses for the customer."
+    initial_message = "Hi, the microwave I bought last week stopped working"
+
+    messages = chatbot_client.initialize_conversation(initial_message, system_message)
+
+    return {"messages": messages}
 
 
-# @app.post("/feedback")
+# @route.post("/feedback")
 # def generate_feedback(messages: list[Message] = []) -> dict[str, Message]:
 #     feedback_message = None
 #     return {"feedback_message": feedback_message}
